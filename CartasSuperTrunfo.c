ba57c614b199d20dc269ccc2c80d3584dffd0b57
GorilaKingui <tugorilasabroso@gmail.com>
2025-09-27 02:40:45 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,36 +2,59 @@
 
 int main(){
 char estado;
-char codigo da carta[50]
-char nome da cidade[50]
-int populaçao
-float area em km
-float pib
-int numeros de pontos turisticos
+char codigo_da_carta[50];
+char nome_da_cidade[50];
+int populaçao;
+float area_em_km;
+float pib;
+int numeros_de_pontos_turisticos;
 
 printf("Casanare: \n");
-scanf("%c", &estado);
+scanf("%s", &estado);
 
 printf("A01: \n");
-scanf("%s", &codigo da carta[50]);
+scanf("%s", &codigo_da_carta[50]);
 
 printf("Yopal: \n");
-scanf("%s", &nome da cidade[50]);
+scanf("%s", &nome_da_cidade[50]);
 
 printf("481.938: \n");
-scanf("%d", &populaçao)
+scanf("%d", &populaçao);
 
-prinf("44.490: \n");
-scanf("%f", &populaçao);
+printf("44.490: \n");
+scanf("%f", &area_em_km);
 
 printf("1500: \n");
 scanf("%f", &pib);
 
 printf("30: \n");
-scanf("%d", &numeros de pontos turisticos)
+scanf("%d", &numeros_de_pontos_turisticos);
+
+printf("Segunda Carta Super Trunfo:");
+  
+printf("Espirito Santo: \n");
+scanf("%s", &estado);
+
+printf("B02: \n");
+scanf("%s", &codigo_da_carta[50]);
+
+printf("Vitoria: \n");
+scanf("%s", &nome_da_cidade[50]);
+
+printf("4126854: \n");
+scanf("%d", &populaçao);
+
+prinf("46095: \n");
+scanf("%f", &area_em_km);
+
+printf("51.1: \n");
+scanf("%f", &pib);
 
 
+printf("29: \n");
+scanf("%d", &numeros_de_pontos_turisticos);  
 
 
+return 0;
 
 }
